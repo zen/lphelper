cdb75b6a74f1d01f300d83a8f01865c0015ad1f8
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-11-14 09:42:02 +0100

    Bug description popup added

--- src/lphelper.js
+++ src/lphelper.js
@@ -12,9 +12,37 @@
 var APIDomain = 'api.launchpad.net';
 var APIUrl = 'https://' + APIDomain + '/1.0/';
 
+var urlRe = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
+
+var urlify = function(str) {
+    return str.replace(urlRe, function(s) {
+        // Remove endings that usually break urls
+        var badEndings = ['.', ',', ';'],
+            ending = '';
+
+        if (badEndings.indexOf(s.slice(-1)) > -1) {
+            ending = s.slice(-1);
+            s = s.slice(0, -1);
+        }
+
+        return '<a href="' + s + '">' + s + '</a>' + ending;
+    });
+};
+
+var cluetipOptions = function() {
+    return {
+        splitTitle: '|',
+        showTitle: false,
+        sticky: true,
+        hoverIntent: false,
+        mouseOutClose: true
+    };
+};
+
 $(document).ready(function() {
+    // Person info
     $(document).find('a.sprite.person')
-        .each(function(idx) {
+        .each(function() {
             var $el = $(this),
                 href = $el.attr('href'),
                 username = href.replace('https://launchpad.net/~', ''),
@@ -34,15 +62,33 @@ $(document).ready(function() {
                     return '<a href="' + entry.web_link + '">' + entry.display_name + '</a>';
                 });
                 $el.attr('title', teams.join('<br />'));
-                $el.cluetip({
-                    splitTitle: '|',
-                    showTitle: false,
-                    sticky: true,
-                    hoverIntent: false,
-                    mouseOutClose: true
-                });
+                $el.cluetip(cluetipOptions());
             }).fail(function(r) {
                 console.log('error', r.status, r.statusText);
+            });
+        });
+
+    // Bug info
+    $(document).find('.buglisting-row .buginfo')
+        .each(function() {
+            var $el = $(this),
+                $ela = $el.find('a'),
+                bugnumber = $el.find('.bugnumber').text().replace('#', ''),
+                url = APIUrl + 'bugs/' + bugnumber;
+
+            $.ajax({
+                method: 'GET',
+                url: url,
+                headers: {'Content-Type': 'application/json'}
+            }).done(function(response) {
+                var opts = cluetipOptions();
+                opts.width = 600;
+
+                var description = response.description.replace(/\n/g, '<br />');
+                description = urlify(description);
+
+                $ela.attr('title', description);
+                $ela.cluetip(opts);
             });
         });
 });
